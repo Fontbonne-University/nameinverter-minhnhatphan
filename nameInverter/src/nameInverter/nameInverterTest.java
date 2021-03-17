7e281662defb57e67037f98b3d1ded8c67589d7b
Minh Phan <nminh238@gmail.com>
2021-03-17 13:17:12 +0700

    module 9

--- nameInverter/src/nameInverter/nameInverterTest.java
+++ nameInverter/src/nameInverter/nameInverterTest.java
@@ -5,16 +5,64 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class nameInverterTest {
-
+	private NameInverter nameInverter;
+	
 	@Before
 	public void setUp() throws Exception {
+		nameInverter = new NameInverter();
 	}
 
 	@Test
-	public void nothing() {
-
+	public void givenNullReturnEmptyString() throws Exception {		
+		assertInverted(null, "");
+	}
+	
+	@Test
+	public void givenEmptyStringReturnEmptyString() throws Exception {
+		assertInverted("", "");
+	}
+	
+	@Test
+	public void givenSimpleNameReturnSimpleName() throws Exception {
+		assertInverted("Name", "Name");
+	}
+	
+	@Test
+	public void givenFirstLastReturnLastFirst() throws Exception {
+		assertInverted("First Last", "Last, First");
+	}
+	
+	@Test
+	public void givenASpimpleNameWithSpacesReturnSimpleNameWithoutSpaces() throws Exception {
+		assertInverted("   Name  ", "Name");
+	}
+	
+	@Test
+	public void givenFirstLastWithExtraSpacesReturnLastFirst() throws Exception {
+		assertInverted("   First   Last   ", "Last, First");
+	}
+	
+	@Test
+	public void ignoreHornorific() throws Exception {
+		assertInverted("Mr. First Last", "Last, First");
+		assertInverted("Mrs. First Last", "Last, First");
+	}
+	
+	@Test
+	public void postNominalsStayAtEnd() throws Exception {
+		assertInverted("First Last Sr.", "Last, First Sr.");
+		assertInverted("First Last BS. Phd.", "Last, First BS. Phd.");
+	}
+	
+	@Test
+	public void intergration() throws Exception {
+		assertInverted("   Robert   Martin    III   esq.   ", "Martin, Robert III esq.");
+	}
+	
+	private void assertInverted(String originalName, String invertedName) {
+		assertEquals(invertedName, nameInverter.invertName(originalName));
 	}
 
+	
 }
